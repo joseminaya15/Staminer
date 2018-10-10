8d61112385e14fe1dd796e50011567cc21669427
joseminaya15 <jose.minayac15@gmail.com>
2018-10-09 23:08:12 -0500

    cropper instalado

--- public/js/jsperfil.js
+++ public/js/jsperfil.js
@@ -2,9 +2,20 @@ function editInformation(){
 	$('.jm-button--first').addClass('jm-opacity--done');
 	$('.jm-button--second').addClass('jm-opacity--none');
 	$('.jm-edit').addClass('jm-opacity--none');
+	$('#informacion-oculta').show();
 }
 function saveInformation(){
 	$('.jm-edit').removeClass('jm-opacity--none');
 	$('.jm-button--first').removeClass('jm-opacity--done');
 	$('.jm-button--second').removeClass('jm-opacity--none');
+	$('#informacion-oculta').hide();
 }
+$('.jm-line').keydown(function(){
+	var textarea = $(this)[0];
+	textarea.style.height = 'auto';
+	textarea.style.height = textarea.scrollHeight + 'px';
+	// if( $('textarea').height() >= '54px'){
+	// 	$('.jm-line').css('height','54px !important');
+	// 	console.log("entra");
+	// }
+});

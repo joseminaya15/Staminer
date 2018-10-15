be296a4d2e5b1a885a2adacf84cb9ddee928f2a5
joseminaya15 <jose.minayac15@gmail.com>
2018-10-15 12:33:06 -0500

    git home terminado

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+	$('#principal .owl-carousel').on('changed.owl.carousel', function(e) {
+		var items     = e.item.count;
+		var item      = e.item.index;
+		$('#numberItem').text('0'+(item-2));
+		$('#numberLength').text('0'+items);
+	});
+	$('.owl-nav').append('<div class="owl-number"><span id="numberItem">01</span>/<span id="numberLength">05</span></div>');
+})
 $('#principal .owl-carousel').owlCarousel({
 	lazyLoad : true,
 	animateOut: 'fadeOut',
@@ -10,6 +19,7 @@ $('#principal .owl-carousel').owlCarousel({
 	navigation : false,
 	nav : true,
 	dots: false,
+	info: true,
 	loop : true,
 	autoplay : true,
 	mouseDrag: false,
@@ -17,7 +27,11 @@ $('#principal .owl-carousel').owlCarousel({
 	navText: ['<i class="mdi mdi-arrow_back"></i>','<i class="mdi mdi-arrow_forward"></i>'],
   	navElement: 'span',
 });
-
+function callback(event) {
+    var items     = event.item.count;     // Number of items
+    var item      = event.item.index;     // Position of the current item
+	console.log(items);
+}
 function goToSolicitud(){
 	$('#solicitud1').addClass('jm-opacity-block');
 	$('#solicitud2').removeClass('jm-opacity');

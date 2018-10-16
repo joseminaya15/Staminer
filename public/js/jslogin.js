6c69f49938716656540ac4bae915dea9023ab918
joseminaya15 <jose.minayac15@gmail.com>
2018-10-16 07:48:18 -0500

    show pass

--- public/js/jslogin.js
+++ public/js/jslogin.js
@@ -1,3 +1,12 @@
 function goToFeed(){
     window.location.href = "feed.html";
 }
+$(".jm-show").click(function() {
+	$(this).find('i').toggleClass("st-mostrar st-editar");
+    var input = $(this).parent().find('.form-control');
+    if (input.attr("type") == "password") {
+    	input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+});

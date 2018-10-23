5f425469fbb2cca112dc3eb924a08765bed9a53a
joseminaya15 <jose.minayac15@gmail.com>
2018-10-22 21:28:24 -0500

    pendientes del trello

--- public/js/jslogin.js
+++ public/js/jslogin.js
@@ -2,7 +2,7 @@ function goToFeed(){
     window.location.href = "feed.html";
 }
 $(".jm-show").click(function() {
-	$(this).find('i').toggleClass("st-mostrar st-editar");
+	$(this).find('i').toggleClass("active");
     var input = $(this).parent().find('.form-control');
     if (input.attr("type") == "password") {
     	input.attr("type", "text");
@@ -10,3 +10,13 @@ $(".jm-show").click(function() {
       input.attr("type", "password");
     }
 });
+function sendPassword(){
+    var correo = $('#correo_password').val();
+    if( correo == null || correo == "" ){
+        msj('error', 'Escriba su correo');
+	    return;
+    }else{
+        msj('error', 'Su contraseña ha sido enviada');
+    }
+    // $('#ModalForgetPassword').modal('hide');
+}
